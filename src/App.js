534956fe6dcbbbc622876f15604bf1a8fb29864a
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-03-20 12:11:24 -0700

    Add & import weather js into main app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>test</h1>
-      </header>
+      <Weather />
     </div>
   );
 }
