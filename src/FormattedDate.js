73beeeded1998ab71baf6e27012a1918b4677821
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 18:36:34 -0700

    Created days, hour and minute strings

---
+++ src/FormattedDate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FormattedDate (props){
+let days =["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
+    let day = days[props.data.getDay()];
+    let hours = props.data.getHours();
+    if (hours <10){
+        hours = `0${hours}`;
+    }
+    let minutes =props.date.getMinutes();
+    if (minutes <10){
+        minutes = `0${minutes}`;
+    }
+    return <div>{day} {hours}:{minutes}</div>;
+}
