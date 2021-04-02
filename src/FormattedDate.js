d53430d4d2ece41e744031a8b0267f7d1f58b8e6
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 18:48:37 -0700

    Removed months for now due to date not showing

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 export default function FormattedDate (props){
-    let days =["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
-    let months = ["Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"]
+    let days =["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    
     let day = days[props.date.getDay()];
-    let month = months[props.date.getMonth()];
     let hours = props.date.getHours();
     if (hours <10){
         hours = `0${hours}`;
@@ -13,5 +12,5 @@ export default function FormattedDate (props){
     if (minutes <10){
         minutes = `0${minutes}`;
     }
-    return <div>{day}, {month} {hours}:{minutes}</div>;
+    return <div>{day} {hours}:{minutes}</div>;
 }
