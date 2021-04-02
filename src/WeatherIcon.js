07b30a344426a66399dc50f045a4871bf8001b9c
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 20:30:30 -0700

    Upgrade size of icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props){
       <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color= "#FFFFFF"
-      size={props.size}
+      size= {150}
       animate = {true}
       />
   );
