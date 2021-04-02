ab5c518c0790d50c5af93b6f2caf4edee9e5d298
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 20:54:46 -0700

    Created unit conversation functions

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function WeatherTemperature (props){
-    return (
+    const [unit, setUnit] = useState ("celsius");
+
+    function convertToFahrenheit (event){
+       event.preventDefault();
+       setUnit("fahrenheit"); 
+    }
+
+    function convertToCelsius (event){
+        event.preventDefault();
+        setUnit("celsius");
+
+    }
+
+    if (unit === 'celsius'){
+        return (
         <div className= "WeatherTemp">
             <p className="currentTemperature">
         <span id="current-temperature">{Math.round(props.celsius)}</span>
         <span className="celsius">
           <a href="some/valid/uri" id="celsius-link" className="active">
+            °C |
+          </a>
+        </span>
+        <span className="fahrenheit">
+          <a href="some/valid.uri" id="fahrenheit-link" onClick={convertToFahrenheit}>
+            {" "}
+            °F{" "}
+          </a>
+        </span>
+      </p>
+        </div>
+        );
+    }else{
+        let fahrenheit = (props.celsius *9)/5+32;
+        return(
+            <div className= "WeatherTemp">
+            <p className="currentTemperature">
+        <span id="current-temperature">{Math.round(fahrenheit)}</span>
+        <span className="celsius">
+          <a href="some/valid/uri" id="celsius-link" className="active" onClick={convertToCelsius}>
             °C |
           </a>
         </span>
@@ -18,5 +52,6 @@ export default function WeatherTemperature (props){
         </span>
       </p>
         </div>
-    );
+        );
+    }
 }
