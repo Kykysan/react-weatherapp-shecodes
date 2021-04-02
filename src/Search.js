879d484cab15454132c72930a818d0ccd16e9a50
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 19:17:06 -0700

    Created search functions

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Search.css";
 
-export default function Search() {
+export default function Search(props) {
+  const[city, setCity] = useState(props.defaultCity);
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+
+  function handleCityChange(event){ 
+    setCity(event.target.value);
+  }
   return (
-    <form className="searchEngine" id="search-engine">
+    <form className="searchEngine" id="search-engine" onSubmit={handleSubmit}>
       <input
         type="search"
         id="search-input"
         placeholder="Enter City"
-        autofocus
+        autofocus="on"
+        onChange={handleCityChange}
       />
       <button className="searchButton" type="submit">
         <i class="fas fa-search-location"></i>
