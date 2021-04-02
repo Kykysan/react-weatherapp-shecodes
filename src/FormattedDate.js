54fbd16a89e8103f19c46fa77634853ed567bf01
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-01 18:45:30 -0700

    Added Month to Timestamp

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function FormattedDate (props){
     let days =["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
+    let months = ["Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"]
     let day = days[props.date.getDay()];
+    let month = months[props.date.getMonth()];
     let hours = props.date.getHours();
     if (hours <10){
         hours = `0${hours}`;
@@ -11,5 +13,5 @@ export default function FormattedDate (props){
     if (minutes <10){
         minutes = `0${minutes}`;
     }
-    return <div>{day} {hours}:{minutes}</div>;
+    return <div>{day}, {month} {hours}:{minutes}</div>;
 }
