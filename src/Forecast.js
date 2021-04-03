ecffb55fb68e4f816a2067207c0dbb58710fd19f
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-03 10:57:11 -0700

    Added index for 5 day forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -16,9 +16,14 @@ export default function Forecast(props) {
     return(
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function(dailyForecast,index){return(
-            <div className="col"><WeatherForecastDay data={dailyForecast} /></div>
-          )})}
+          {forecast.map(function(dailyForecast,index){
+            if(index <5){
+            return(
+            <div className="col" key={index}>
+              <WeatherForecastDay data={dailyForecast} />
+              </div>
+          );}
+          })}
         </div>
       </div>
     );
