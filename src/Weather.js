1ef095d27113257700acebf4f3c0c8371fd52861
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-03 10:01:48 -0700

    Added coord to weatherdata

--- src/Weather.js
+++ src/Weather.js
@@ -18,6 +18,7 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       date: new Date(response.data.dt*1000),
       icon: response.data.weather[0].icon,
+      coord: response.data.coord,
       ready: true,
     });  
   }
